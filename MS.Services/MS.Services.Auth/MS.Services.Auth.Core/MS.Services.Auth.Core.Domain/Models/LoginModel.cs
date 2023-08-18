0ad9927e51e3c7fbbeb4788d5759c55ef2781d24
Lucas Everton Santos de Oliveira <lucas.oliveira-ext@fiven.com>
2023-08-18 16:54:16 -0300

    Commit

---
+++ MS.Services/MS.Services.Auth/MS.Services.Auth.Core/MS.Services.Auth.Core.Domain/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MS.Services.Auth.Core.Domain.Models;
+
+public class LoginModel
+{
+    public string Login { get; set; }
+
+    public string Password { get; set; }
+}
---
+++ MS.Services/MS.Services.Auth/MS.Services.Auth.Core/MS.Services.Auth.Core.Domain/Models/TokenModel.cs
@@ -0,0 +1,10 @@
+﻿using MS.Libs.Core.Domain.Models.Base;
+
+namespace MS.Services.Auth.Core.Domain.Models;
+
+public class TokenModel : BaseModel
+{
+    public string Token { get; set; }
+
+    public DateTime Expiration { get; set; }
+}
