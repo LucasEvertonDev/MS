c152f413c3ba5dcb75ca8a5e61b8489bd1440912
Lcs Everton <lcseverton@outlook.com>
2023-08-18 21:09:28 -0300

    upload docker file

--- MS.Services/MS.Services.Auth/MS.Services.Auth.WebAPI/Controllers/AuthController.cs
+++ MS.Services/MS.Services.Auth/MS.Services.Auth.WebAPI/Controllers/AuthController.cs
@@ -1,40 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MS.Libs.WebApi.Controllers;
 using MS.Libs.WebApi.HttpContainers;
-using MS.Services.Auth.Core.Domain.Models;
 
 namespace MS.Services.Auth.WebAPI.Controllers
 {
     [Route("api/v2/[Controller]")]
     public class AuthController : BaseController
     {
-        [HttpPost("register")]
-        [ProducesResponseType(typeof(ResponseDTO<TokenModel>), StatusCodes.Status200OK)]
-        public ActionResult Register([FromBody] RequestDTO<LoginModel> requestDTO)
-        {
-            var response = new ResponseDTO<TokenModel>()
-            {
-                Sucess = true
-            };
+        //[HttpPost("register")]
+        //[ProducesResponseType(typeof(ResponseDTO<TokenModel>), StatusCodes.Status200OK)]
+        //public ActionResult Register([FromBody] RequestDTO<LoginModel> requestDTO)
+        //{
+        //    var response = new ResponseDTO<TokenModel>()
+        //    {
+        //        Sucess = true
+        //    };
 
-            return Ok(response);
-        }
+        //    return Ok(response);
+        //}
 
-        /// <summary>
-        /// Rota para logar com um usuário
-        /// </summary>
-        /// <param name="requestDTO"></param>
-        /// <returns></returns>
-        [HttpPost("login")]
-        [ProducesResponseType(typeof(ResponseDTO<TokenModel>), StatusCodes.Status200OK)]
-        public ActionResult Login([FromBody] RequestDTO<LoginModel> requestDTO)
-        {
-            var response = new ResponseDTO<TokenModel>()
-            {
-                Sucess = true
-            };
+        ///// <summary>
+        ///// Rota para logar com um usuário
+        ///// </summary>
+        ///// <param name="requestDTO"></param>
+        ///// <returns></returns>
+        //[HttpPost("login")]
+        //[ProducesResponseType(typeof(ResponseDTO<TokenModel>), StatusCodes.Status200OK)]
+        //public ActionResult Login([FromBody] RequestDTO<LoginModel> requestDTO)
+        //{
+        //    var response = new ResponseDTO<TokenModel>()
+        //    {
+        //        Sucess = true
+        //    };
 
-            return Ok(response);
-        }
+        //    return Ok(response);
+        //}
     }
 }
