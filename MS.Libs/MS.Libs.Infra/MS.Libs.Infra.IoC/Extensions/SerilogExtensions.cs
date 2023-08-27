87f8868dce2e6544dcbb7619975ca10e1bda05a5
Lcs Everton <lcseverton@outlook.com>
2023-08-27 13:20:19 -0300

    Upload config

--- MS.Libs/MS.Libs.Infra/MS.Libs.Infra.IoC/Extensions/SerilogExtensions.cs
+++ MS.Libs/MS.Libs.Infra/MS.Libs.Infra.IoC/Extensions/SerilogExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Azure.Core;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using MS.Libs.Core.Domain.Infra.AppSettings;
@@ -10,8 +9,6 @@ using Serilog.Core;
 using Serilog.Events;
 using Serilog.Sinks.MSSqlServer;
 using System.Collections.ObjectModel;
-using System.Configuration;
-using System.Text;
 using Log = Serilog.Log;
 
 namespace MS.Libs.Infra.IoC.Extensions;
@@ -20,8 +17,6 @@ public static class SerilogExtensions
 {
     public static void RegisterSerilog(this HostBuilderContext app, IConfigurationBuilder configurationBuilder)
     {
-        // app.HostingEnvironment.IsDevelopment()
-
         var settings = configurationBuilder.Build();
 
         var appSettings = new AppSettings(settings);
@@ -37,8 +32,15 @@ public static class SerilogExtensions
             {
                 new SqlColumn { ColumnName = "ClientId", DataLength=  50, DataType = System.Data.SqlDbType.NVarChar },
                 new SqlColumn { ColumnName = "UserId", DataLength=  50, DataType = System.Data.SqlDbType.NVarChar },
-                new SqlColumn { ColumnName = "Request", DataLength=  -1, DataType = System.Data.SqlDbType.NVarChar },
-            }
+            },
+            Message = new ColumnOptions.MessageColumnOptions
+            {
+                DataLength = 1000
+            },
+            MessageTemplate = new ColumnOptions.MessageTemplateColumnOptions
+            {
+                DataLength = 1000
+            },
         };
 
         Log.Logger = new LoggerConfiguration()
@@ -82,128 +84,12 @@ public class UserEnricher : ILogEventEnricher
 
     public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
     {
-        var x = logEvent.Level;
-
         logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
                 "UserId", _httpContextAccessor.HttpContext?.User?.Identity.GetUserClaim(JWTUserClaims.UserId) ?? "anonymous"));
 
 
         logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
                 "ClientId", _httpContextAccessor.HttpContext?.User?.Identity.GetUserClaim(JWTUserClaims.ClientId) ?? "anonymous"));
-
-    }
-
-    private LogEventProperty CreateProperty(ILogEventPropertyFactory propertyFactory, HttpContext httpContext)
-    {
-        var httpRequest = httpContext.Request;
-
-        string body = "";
-
-        Task.WaitAll(
-            Task.Run(async () =>
-            {
-                if (httpRequest.ContentLength.HasValue && httpRequest.ContentLength > 0)
-                {
-                    string aux = await httpRequest.GetRawBodyAsync();
-
-                    body = aux;
-                }
-            }));
-        
-
-        var httpRequestInfo = new HttpRequestInfo()
-        {
-            Host = httpRequest.Host.ToString(),
-            Path = httpRequest.Path,
-            Scheme = httpRequest.Scheme,
-            Method = httpRequest.Method,
-            Protocol = httpRequest.Protocol,
-            QueryString = httpRequest.Query.ToDictionary(x => x.Key, y => y.Value.ToString()),
-            Headers = httpRequest.Headers
-                        .Where(x => x.Key != "Cookie" && x.Key != "Authorization") // remove Cookie from header since it is analysed separatly
-                        .ToDictionary(x => x.Key, y => y.Value.ToString()),
-            Cookies = httpRequest.Cookies.ToDictionary(x => x.Key, y => y.Value.ToString()),
-            Body = body
-        };
-
-        var httpRequestProperty = propertyFactory.CreateProperty("Request", httpRequestInfo, true);
-
-        return httpRequestProperty;
-    }
-
-    /// <summary>
-    /// Helper class to run async methods within a sync process.
-    /// </summary>
-    internal static class AsyncUtil
-    {
-        private static readonly TaskFactory _taskFactory = new TaskFactory(CancellationToken.None, TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
-
-        /// <summary>
-        /// Executes an async Task method which has a void return value synchronously
-        /// USAGE: AsyncUtil.RunSync(() => AsyncMethod());
-        /// </summary>
-        /// <param name="task">Task method to execute</param>
-        public static void RunSync(Func<Task> task)
-            => _taskFactory
-                .StartNew(task)
-                .Unwrap()
-                .GetAwaiter()
-                .GetResult();
-
-        /// <summary>
-        /// Executes an async Task<T> method which has a T return type synchronously
-        /// USAGE: T result = AsyncUtil.RunSync(() => AsyncMethod<T>());
-        /// </summary>
-        /// <typeparam name="TResult">Return Type</typeparam>
-        /// <param name="task">Task<T> method to execute</param>
-        /// <returns></returns>
-        public static TResult RunSync<TResult>(Func<Task<TResult>> task)
-            => _taskFactory
-                .StartNew(task)
-                .Unwrap()
-                .GetAwaiter()
-                .GetResult();
     }
 }
 
-public static class Teste
-{
-    public static async Task<string> GetRawBodyAsync(
-       this HttpRequest request,
-       Encoding encoding = null)
-    {
-        if (!request.Body.CanSeek)
-        {
-            // We only do this if the stream isn't *already* seekable,
-            // as EnableBuffering will create a new stream instance
-            // each time it's called
-            request.EnableBuffering();
-        }
-
-        var reader = new StreamReader(request.Body, encoding ?? Encoding.UTF8);
-
-        var body = await reader.ReadToEndAsync().ConfigureAwait(false);
-
-        request.Body.Position = 0;
-
-        return body;
-    }
-
-}
-
-internal class HttpRequestInfo
-{
-    public HttpRequestInfo()
-    {
-    }
-
-    public string Host { get; set; }
-    public PathString Path { get; set; }
-    public string Scheme { get; set; }
-    public string Method { get; set; }
-    public string Protocol { get; set; }
-    public Dictionary<string, string> QueryString { get; set; }
-    public Dictionary<string, string> Headers { get; set; }
-    public Dictionary<string, string> Cookies { get; set; }
-    public string Body { get; set; }
-}
--- MS.Services/MS.Services.Auth/MS.Services.Auth.WebAPI/Infrastructure/Startup.cs
+++ MS.Services/MS.Services.Auth/MS.Services.Auth.WebAPI/Infrastructure/Startup.cs
@@ -139,29 +139,3 @@ public class Startup
         });
     }
 }
-
-
-public static class Teste
-{
-    public static async Task<string> GetRawBodyAsync(
-       this HttpRequest request,
-       Encoding encoding = null)
-    {
-        if (!request.Body.CanSeek)
-        {
-            // We only do this if the stream isn't *already* seekable,
-            // as EnableBuffering will create a new stream instance
-            // each time it's called
-            request.EnableBuffering();
-        }
-
-        var reader = new StreamReader(request.Body, encoding ?? Encoding.UTF8);
-
-        var body = await reader.ReadToEndAsync().ConfigureAwait(false);
-
-        request.Body.Position = 0;
-
-        return body;
-    }
-
-}
