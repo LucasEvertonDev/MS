7899f38ea93ee24381b2d53c35dd563ede668616
Lcs Everton <lcseverton@outlook.com>
2023-09-03 18:50:24 -0300

    validação de senhas iguais

--- MS.WebUI/control-services/src/app/features/auth/components/register/register.component.ts
+++ MS.WebUI/control-services/src/app/features/auth/components/register/register.component.ts
@@ -21,38 +21,28 @@ export class RegisterComponent implements OnInit {
     this.formRegister = this.formBuilder.group<FormRegister>({
       username: new FormControl<string>({ value: '', disabled: false }, { nonNullable: true, validators: [Validators.required], },),
       password: new FormControl<string>({ value: '', disabled: false }, { nonNullable: true, validators: Validators.compose([Validators.required, Validators.minLength(4)]) }),
-      comfirmpassword: new FormControl<string>({ value: '', disabled: false }, { nonNullable: true, validators: this.checkPasswords }),
+      comfirmpassword: new FormControl<string>({ value: '', disabled: false }, { nonNullable: true}),
       email: new FormControl<string>({ value: '', disabled: false }, { nonNullable: true, validators: Validators.compose([ Validators.compose([Validators.required, Validators.email])]) }),
       name: new FormControl<string>({ value: '', disabled: false }, { nonNullable: true, validators: Validators.compose([Validators.required]) }),
       userGroupId: new FormControl<string>({ value: "F97E565D-08AF-4281-BC11-C0206EAE06FA", disabled: true }, { nonNullable: true, validators: Validators.compose([Validators.required]) }),
     });
+
+    this.formRegister.addValidators(this.comparisonValidator());
   }
 
   public register(): void {
 
   }
 
-  public checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => { 
-    let pass = group.get('password')?.value ?? "";
-    let confirmPass = group.get('comfirmpassword')?.value ?? "";
-    return pass === confirmPass ? null : { password_not_equals: true }
-  }
-
-   ConfirmedValidator(controlName: string, matchingControlName: string) {
-    return (formGroup: FormGroup) => {
-      const control = formGroup.controls[controlName];
-      const matchingControl = formGroup.controls[matchingControlName];
-      if (
-        matchingControl.errors &&
-        !matchingControl.errors.confirmedValidator
-      ) {
-        return;
-      }
-      if (control.value !== matchingControl.value) {
-        matchingControl.setErrors({ confirmedValidator: true });
-      } else {
-        matchingControl.setErrors(null);
-      }
+  public comparisonValidator() : any{
+    return (group: FormGroup): any => {
+       const control1 = group.controls['password'];
+       const control2 = group.controls['comfirmpassword'];
+       if (control1.value !== control2.value) {
+          control2.setErrors({notEquivalent: true});
+       } else {
+          control2.setErrors(null);
+       }
     };
   }
 }
